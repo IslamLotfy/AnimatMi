aee691a50e372a8772ca1e6c86b300eef5826faa
Islam <islam.m7md.lotfy@gmail.com>
2020-04-07 17:17:43 +0200

    first run done

--- app/src/main/java/com/example/animatmi/MainActivity.kt
+++ app/src/main/java/com/example/animatmi/MainActivity.kt
@@ -1,24 +1,17 @@
 package com.example.animatmi
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
